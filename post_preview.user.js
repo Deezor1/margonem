64b70a6baa92c4bf21865d5c03843b2d8f2bae90
Deezor <42243233+Deezor1@users.noreply.github.com>
2019-04-08 17:35:04 +0200

    Update post_preview.user.js

--- post_preview.user.js
+++ post_preview.user.js
@@ -12,7 +12,6 @@
     if(b.innerHTML.indexOf("i") == 1){
         const but = document.createElement("button");
         but.style.cssText = 'width: 200px; height: 30px';
-        but.id = 'postPrev';
         const t = document.createTextNode('Podgląd postu');
         but.appendChild(t);
         b.appendChild(but);
@@ -21,29 +20,29 @@
         b.appendChild(d);
         but.addEventListener('click', (e) => {
             e.preventDefault();
-            let c = document.getElementById('content').value;
+            let c = document.querySelector('#content').value;
             c = c.replace(/\n/g, ' <br>');
             c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
             c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
             c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
             c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
             c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
-            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});//¯\_(ツ)_/¯
+            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});//z tego co widziałem to tak samo jest napisane forum ¯\_(ツ)_/¯
             c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});
             c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});
             c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});
             c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});
             c = c.replace(/\[center\]/g, '<center>');
             c = c.replace(/\[\/center\]/g, '</center>');
-            c = c.replace(/\[b\](.+?)\[\/b\]/g, (match, a1) => {return '<strong>' + a1 + '</strong>';});
-            c = c.replace(/\[b\](.+?)\[\/b\]/g, (match, a1) => {return '<strong>' + a1 + '</strong>';});
-            c = c.replace(/\[i\](.+?)\[\/i\]/g, (match, a1) => {return '<em>' + a1 + ' </em>';});
-            c = c.replace(/\[i\](.+?)\[\/i\]/g, (match, a1) => {return '<em>' + a1 + ' </em>';});
-            c = c.replace(/\[u\](.+?)\[\/u\]/g, (match, a1) => {return '<u>' + a1 + ' </u>';});
-            c = c.replace(/\[u\](.+?)\[\/u\]/g, (match, a1) => {return '<u>' + a1 + ' </u>';});
-            c = c.replace(/http:\/\/.*?.margonem.pl\/obrazki\/npc\/(.+?)(?= )/g, (match, a1) => {return '<img src="/obrazki/npc/' + a1 + '">';});
-            c = c.replace(/http:\/\/.*?.margonem.pl\/obrazki\/npc\/(.+?)$/g, (match, a1) => {return '<img src="/obrazki/npc/' + a1 + '">';});
-            c = c.replace(/(?=http:\/\/)(.+?)(?= )|(?=http:\/\/)(.+?)$|(?=https:\/\/)(.+?)(?= )|(?=https:\/\/)(.+?)$/g, ' <a href="$&" target="_blank">$&</a>');
+            c = c.replace(/\[b\](.+?)\[\/b\]/g, (match, a1) => {return ' <strong>' + a1 + ' </strong>';});
+            c = c.replace(/\[b\](.+?)\[\/b\]/g, (match, a1) => {return ' <strong>' + a1 + ' </strong>';});
+            c = c.replace(/\[i\](.+?)\[\/i\]/g, (match, a1) => {return ' <em>' + a1 + ' </em>';});
+            c = c.replace(/\[i\](.+?)\[\/i\]/g, (match, a1) => {return ' <em>' + a1 + ' </em>';});
+            c = c.replace(/\[u\](.+?)\[\/u\]/g, (match, a1) => {return ' <u>' + a1 + ' </u>';});
+            c = c.replace(/\[u\](.+?)\[\/u\]/g, (match, a1) => {return ' <u>' + a1 + ' </u>';});
+            c = c.replace(/http:\/\/.*?.margonem.pl\/obrazki\/npc\/(.+?)(?= )/g, (match, a1) => {return ' <img src="/obrazki/npc/' + a1 + '">';});
+            c = c.replace(/http:\/\/.*?.margonem.pl\/obrazki\/npc\/(.+?)$/g, (match, a1) => {return ' <img src="/obrazki/npc/' + a1 + '">';});
+            c = c.replace(/(?=http:\/\/)(.+?)(?= )|(?=http:\/\/)(.+?)$|(?=https:\/\/)(.+?)(?= )|(?=https:\/\/)(.+?)$/g, '<a href="$&" target="_blank">$&</a>');
             c = c.replace(/ ;\)\)\)/g, ' <img src="/obrazki/emots/lol.gif">');
             c = c.replace(/ ;\)/g, ' <img src="/obrazki/emots/wink.gif">');
             c = c.replace(/ :\)/g, ' <img src="/obrazki/emots/smile.gif">');
@@ -70,7 +69,13 @@
             c = c.replace(/ :\]| ;\]/g, ' <img src="/obrazki/emots/smirk.gif">');
             c = c.replace(/ :\*| ;\*/g, ' <img src="/obrazki/emots/kiss.gif">');
             c = c.replace(/ :rotfl/g, ' <img src="/obrazki/emots/rotfl.gif">');
-            c = c.replace(/ITEM#.+?(?= )|ITEM#.+?$/g, '<img src="https://i.imgur.com/0eOUPIz.png" align="left">');
+            c = c.replace(/ITEM#.+?(?= )|ITEM#.+?$/g, ' <img src="https://i.imgur.com/0eOUPIz.png" align="left">');
+            c = c.replace(/ \<strong\>/g, '<strong>');
+            c = c.replace(/ \<\/strong\>/g, '</strong>');
+            c = c.replace(/ \<em\>/g, '<em>');
+            c = c.replace(/ \<\/em\>/g, '</em>');
+            c = c.replace(/ \<u\>/g, '<u>');
+            c = c.replace(/ \<\/u\>/g, '</u>');
             d.innerHTML = c;
         });
     }
