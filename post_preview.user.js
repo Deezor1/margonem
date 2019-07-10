acabcbc8bd4d2bfe341426b5b2ce8b2375e08098
Deezor <42243233+Deezor1@users.noreply.github.com>
2019-07-10 15:31:09 +0200

    Update post_preview.user.js

--- post_preview.user.js
+++ post_preview.user.js
@@ -14,7 +14,7 @@
     if(b.innerHTML.indexOf("i") == 1 || b.innerHTML.indexOf("f") == 6){
         const but = document.createElement("button");
         but.style.cssText = 'width: 200px; height: 30px';
-        but.appendChild(document.createTextNode('Podgląd postu'));
+        but.appendChild(document.createTextNode('Podgląd'));
         b.appendChild(but);
         const d = document.createElement('div');
         d.style.cssText = 'text-align: left; margin-bottom: 20px;';
@@ -25,25 +25,25 @@
             c = document.querySelector('#content') != null ? c = document.querySelector('#content').value : c;
             c = document.querySelectorAll('textarea')[0].id !== 'content' ? c = document.querySelectorAll('textarea')[0].value : c;
             c = c.replace(/\n/g, ' <br>');
-            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
-            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
-            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
-            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
-            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return '<code>' + a1 + ' </code>';});
-            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});//z tego co widziałem to tak samo jest napisane forum ¯\_(ツ)_/¯
-            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});
-            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});
-            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});
-            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return '<blockquote>' + a1 + ' </blockquote>';});
-            c = c.replace(/\[center\](.+?)\[\/center\]/g, (match, a1) => {return '<center>' + a1 + ' </center>';});
-            c = c.replace(/\[b\](.+?)\[\/b\]/g, (match, a1) => {return ' <strong>' + a1 + ' </strong>';});
-            c = c.replace(/\[b\](.+?)\[\/b\]/g, (match, a1) => {return ' <strong>' + a1 + ' </strong>';});
-            c = c.replace(/\[i\](.+?)\[\/i\]/g, (match, a1) => {return ' <em>' + a1 + ' </em>';});
-            c = c.replace(/\[i\](.+?)\[\/i\]/g, (match, a1) => {return ' <em>' + a1 + ' </em>';});
-            c = c.replace(/\[u\](.+?)\[\/u\]/g, (match, a1) => {return ' <u>' + a1 + ' </u>';});
-            c = c.replace(/\[u\](.+?)\[\/u\]/g, (match, a1) => {return ' <u>' + a1 + ' </u>';});
-            c = c.replace(/http:\/\/.*?.margonem.pl\/obrazki\/npc\/(.+?)(?= )/g, (match, a1) => {return ' <img src="/obrazki/npc/' + a1 + '">';});
-            c = c.replace(/http:\/\/.*?.margonem.pl\/obrazki\/npc\/(.+?)$/g, (match, a1) => {return ' <img src="/obrazki/npc/' + a1 + '">';});
+            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return `<code>${a1}</code>`;});
+            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return `<code>${a1}</code>`;});
+            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return `<code>${a1}</code>`;});
+            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return `<code>${a1}</code>`;});
+            c = c.replace(/\[code\](.+?)\[\/code\]/g, (match, a1) => {return `<code>${a1}</code>`;});
+            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return `<blockquote>${a1}</blockquote>`;});//z tego co widziałem to tak samo jest napisane forum ¯\_(ツ)_/¯
+            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return `<blockquote>${a1}</blockquote>`;});
+            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return `<blockquote>${a1}</blockquote>`;});
+            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return `<blockquote>${a1}</blockquote>`;});
+            c = c.replace(/\[cytat\](.+?)\[\/cytat\]/g, (match, a1) => {return `<blockquote>${a1}</blockquote>`;});
+            c = c.replace(/\[center\](.+?)\[\/center\]/g, (match, a1) => {return `<center>${a1}</center>`;});
+            c = c.replace(/\[b\](.+?)\[\/b\]/g, (match, a1) => {return ` <strong>${a1}</strong>`;});
+            c = c.replace(/\[b\](.+?)\[\/b\]/g, (match, a1) => {return ` <strong>${a1}</strong>`;});
+            c = c.replace(/\[i\](.+?)\[\/i\]/g, (match, a1) => {return ` <em>${a1}</em>`;});
+            c = c.replace(/\[i\](.+?)\[\/i\]/g, (match, a1) => {return ` <em>${a1}</em>`;});
+            c = c.replace(/\[u\](.+?)\[\/u\]/g, (match, a1) => {return ` <u>${a1}</u>`;});
+            c = c.replace(/\[u\](.+?)\[\/u\]/g, (match, a1) => {return ` <u>${a1}</u>`;});
+            c = c.replace(/http:\/\/.*?.margonem.pl\/obrazki\/npc\/(.+?)(?= )/g, (match, a1) => {return ` <img src="/obrazki/npc/${a1}">`;});
+            c = c.replace(/http:\/\/.*?.margonem.pl\/obrazki\/npc\/(.+?)$/g, (match, a1) => {return ` <img src="/obrazki/npc/${a1}">`;});
             c = c.replace(/(?=http:\/\/)(.+?)(?= )|(?=http:\/\/)(.+?)$|(?=https:\/\/)(.+?)(?= )|(?=https:\/\/)(.+?)$/g, '<a href="$&" target="_blank">$&</a>');
             c = c.replace(/ ;\)\)\)/g, ' <img src="/obrazki/emots/lol.gif">');
             c = c.replace(/ ;\)/g, ' <img src="/obrazki/emots/wink.gif">');
@@ -71,7 +71,7 @@
             c = c.replace(/ :\]| ;\]/g, ' <img src="/obrazki/emots/smirk.gif">');
             c = c.replace(/ :\*| ;\*/g, ' <img src="/obrazki/emots/kiss.gif">');
             c = c.replace(/ :rotfl/g, ' <img src="/obrazki/emots/rotfl.gif">');
-            c = c.replace(/ITEM#.+?(?= )|ITEM#.+?$/g, ' <img src="https://i.imgur.com/0eOUPIz.png" align="left">');
+            c = c.replace(/ITEM#.+?(?= )|ITEM#.+?$/g, ' <img src="https://raw.githubusercontent.com/Deezor1/margonem/master/item_template.png" align="left">');
             c = c.replace(/ \<strong\>/g, '<strong>');
             c = c.replace(/ \<\/strong\>/g, '</strong>');
             c = c.replace(/ \<em\>/g, '<em>');
